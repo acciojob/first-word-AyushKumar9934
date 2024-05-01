e8c208c6a6342e39e229efd9b13b7db353f782f3
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-01 18:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function firstWord(s) {
   // your code here
+	let res="";
+	let i=0;
+	while(i<s.length){
+		if(s[i]==" "){
+   return res;
+		
+		}
+		else res+=s[i];
+		i++;
+	}
+	return res;
 }
-
+  
 // Do not change the code below
 
 const s = prompt("Enter String:");
